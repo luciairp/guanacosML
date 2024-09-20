47bdabb5ca1f107e67128de9a2a5710bfb876bdf
luciairp <luciarp@gmail.com>
2024-09-20 16:57:20 -0300

    Modifico fx "procesar muestreo": - opción estimar individuos o grupos - extraer más resultados en lista b (área cubierta y datos de clusters)

--- ms monitoring L guanicoe Monte León NP/ms_fx.R
+++ ms monitoring L guanicoe Monte León NP/ms_fx.R
@@ -1,9 +1,9 @@
 # funciones para ajustar funciones de detección
 
 # función para ajustar 3 key functions con mismas características
-# hn uniform y hazard rate para grupos de individuos dht_group F
+# hn uniform y hazard rate para calcular individuos usando size dht_group F
 # monotonicity strict, máximo 3 adjustments:
-fit.hn.uni.haz.list.group <- function(data, trunc) {
+fit.hn.uni.haz.list <- function(data, trunc) {
   # Purpose: fit 3 key functions to transect data and print model selection table
   # Input: data to analyse, truncation distance, print flag
   # Output: model object (class `dsmodel`)
@@ -17,6 +17,28 @@ fit.hn.uni.haz.list.group <- function(data, trunc) {
                        max_adjustments = 3, monotonicity = "strict",
                        convert_units = conversion.factor)
   models$haz.poly <- ds(data, trun=trunc, key="hr", adj="poly", dht_group = F,
+                        max_adjustments = 3, monotonicity = "strict",
+                        convert_units = conversion.factor)
+  
+  return(models)
+}
+
+# version calculando densidad de grupos
+# grupos (no individuos) dht_group T
+fit.hn.uni.haz.list.group <- function(data, trunc) {
+  # Purpose: fit 3 key functions to transect data and print model selection table
+  # Input: data to analyse, truncation distance, print flag
+  # Output: model object (class `dsmodel`)
+  # unidades: distancia - esfuerzo - area
+  conversion.factor <- convert_units("Meter", "Kilometer", "Square kilometer")
+  models <- list()
+  models$hn.cos <- ds(data, trun=trunc, key="hn", adj="cos", dht_group = T,
+                      max_adjustments = 3, monotonicity = "strict",
+                      convert_units = conversion.factor)
+  models$uni.cos <- ds(data, trun=trunc, key="unif", adj="cos", dht_group = T,
+                       max_adjustments = 3, monotonicity = "strict",
+                       convert_units = conversion.factor)
+  models$haz.poly <- ds(data, trun=trunc, key="hr", adj="poly", dht_group = T,
                         max_adjustments = 3, monotonicity = "strict",
                         convert_units = conversion.factor)
   
@@ -40,30 +62,47 @@ extract_model_data_table <- function(model){
 
 extract_model_res_table <- function(model){
   # extrae elementos elegidos del resultado de un modelo y arma una tabla
-  out <- as_tibble(matrix(0, nrow = 1, ncol = 11))
-  colnames(out) <-c('Modelo','Región','D_est','SE','Lcl','Ucl','Group_Size_est','SE_grupo','N_est','LclN','UclN')
+  out <- as_tibble(matrix(0, nrow = 1, ncol = 16))
+  colnames(out) <-c('Modelo','Región','AreaCubierta',
+                    'D_est','SE','Lcl','Ucl',
+                    'Group_Size_est','SE_grupo',
+                    'N_est','LclN','UclN',
+                    'gr_est','gr_se','gr_lcl','gr_ucl')
   out[[1]] <- model$call$key
   out[[2]] <- model$dht$clusters$summary$Region
-  out[[3]] <- model$dht$individuals$D$Estimate
-  out[[4]] <- model$dht$individuals$D$se
-  out[[5]] <- model$dht$individuals$D$lcl
-  out[[6]] <- model$dht$individuals$D$ucl
-  out[[7]] <- model$dht$Expected.S$Expected.S
-  out[[8]] <- model$dht$Expected.S$se.Expected.S
-  out[[9]] <- model$dht$individuals$N$Estimate
-  out[[10]] <- model$dht$individuals$N$lcl
-  out[[11]] <- model$dht$individuals$N$ucl
+  out[[3]] <- model$dht$clusters$summary$CoveredArea
+  out[[4]] <- model$dht$individuals$D$Estimate
+  out[[5]] <- model$dht$individuals$D$se
+  out[[6]] <- model$dht$individuals$D$lcl
+  out[[7]] <- model$dht$individuals$D$ucl
+  out[[8]] <- model$dht$Expected.S$Expected.S
+  out[[9]] <- model$dht$Expected.S$se.Expected.S
+  out[[10]] <- model$dht$individuals$N$Estimate
+  out[[11]] <- model$dht$individuals$N$lcl
+  out[[12]] <- model$dht$individuals$N$ucl
+  out[[13]] <- model$dht$clusters$N$Estimate
+  out[[14]] <- model$dht$clusters$N$se
+  out[[15]] <- model$dht$clusters$N$lcl
+  out[[16]] <- model$dht$clusters$N$ucl
   return(out)  
 }
 
-procesar_muestreo <- function(data, muestreo_valor, especie, truncado_valor){
+procesar_muestreo <- function(data, muestreo_valor, especie, 
+                              truncado_valor,usar_grupo = TRUE){
   trans <- data %>% 
     filter(Especie == especie) %>% 
     filter(Muestreo == muestreo_valor)
   par(mfrow = c(1,1),mar=c(3,3,2,1),cex.main=1)
   h <- hist(trans$distance, main = paste("Muestreo ", muestreo_valor, 
                                          "\nDistancia truncada ", truncado_valor,"m"))
-  res <- fit.hn.uni.haz.list.group(trans,truncado_valor)
+  
+  # Selecciona la función a usar según el valor de 'usar_grupo'
+  if (usar_grupo) {
+    res <- fit.hn.uni.haz.list.group(trans, truncado_valor)
+  } else {
+    res <- fit.hn.uni.haz.list(trans, truncado_valor)
+  }
+  
   a <- map_df(res,extract_model_data_table) %>% 
     mutate(Muestreo = muestreo_valor)
   b <- map_df(res,extract_model_res_table) %>% 
